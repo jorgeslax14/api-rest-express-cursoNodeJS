b31bdb35eeec370e3bffbdb4ac8509182ba9d9d0
jorgeslax14 <ingeniero.jorgevelasquez@gmail.com>
2022-01-04 20:04:32 -0500

    Primer despliegue heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const config  = require('config');
 
 //const loging  = require('./middleware/logger.js')
 
-// Middleware
+// Middleware 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
